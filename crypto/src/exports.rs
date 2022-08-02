462f226e6182beb5753feda5f41c6f6eee4e395e
xixi <i@hexilee.me>
2022-08-03 01:09:31 +0800

    complete definition of sign
    Signed-off-by: xixi <i@hexilee.me>

--- crypto/src/exports.rs
+++ crypto/src/exports.rs
@@ -1,2 +1,37 @@
+#[repr(C)]
+pub struct BigInt {
+    pub value: *mut u8,
+
+    pub len: u32,
+
+    // The number base to parse the value as.
+    pub base: u8,
+}
+
+#[repr(C)]
+pub struct ECDocument {
+    pub msg_hash: BigInt,
+    pub private_key: BigInt,
+    pub seed: *const BigInt,
+}
+
+#[repr(C)]
+pub struct ECSignature {
+    pub r: BigInt,
+    pub s: BigInt,
+}
+
+#[repr(C)]
+pub struct Error {
+    pub msg: *mut u8,
+    pub len: u32,
+}
+
 #[no_mangle]
-pub extern "C" fn sign() {}
+pub unsafe extern "C" fn sign(
+    _document: *const ECDocument,
+    _signature: *mut ECSignature,
+    _err_msg: *mut Error,
+) -> i32 {
+    return 0;
+}
