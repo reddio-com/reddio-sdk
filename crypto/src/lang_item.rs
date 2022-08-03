4826cf8844c6636180a8964523c41a09a75c7d49
xixi <i@hexilee.me>
2022-08-03 23:32:50 +0800

    use stdlib
    Signed-off-by: xixi <i@hexilee.me>

--- crypto/src/lang_item.rs
+++
@@ -1,9 +0,0 @@
-use core::panic::PanicInfo;
-
-#[panic_handler]
-fn handle_panic(_: &PanicInfo) -> ! {
-    loop {}
-}
-
-#[lang = "eh_personality"]
-extern "C" fn eh_personality() {}
--- crypto/src/lib.rs
+++ crypto/src/lib.rs
@@ -1,8 +1,2 @@
-#![no_std]
-#![feature(lang_items)]
-
 #[cfg(test)]
 mod tests;
-
-#[cfg(not(test))]
-mod lang_item;
