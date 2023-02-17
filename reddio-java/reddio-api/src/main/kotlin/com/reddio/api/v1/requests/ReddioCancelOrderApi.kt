9779a56722d581dd408865ddfbb13e40f89f3889
Zhou Zhiqiang <im@strrl.dev>
2023-02-17 11:03:00 +0800

    feat(java sdk): new style of cancel order api (#70)
    Signed-off-by: STRRL <im@strrl.dev>

---
+++ reddio-java/reddio-api/src/main/kotlin/com/reddio/api/v1/requests/ReddioCancelOrderApi.kt
@@ -0,0 +1,68 @@
+package com.reddio.api.v1.requests
+
+import com.reddio.api.v1.StarkExSigner
+import com.reddio.api.v1.requests.polling.OrderPoller
+import com.reddio.api.v1.rest.*
+import java.util.concurrent.CompletableFuture
+
+class ReddioCancelOrderApi private constructor(
+    private val localRestClient: ReddioRestClient, private val orderId: Long, private val request: CancelOrderMessage
+) : SignedReddioApiRequest<CancelOrderMessage, ResponseWrapper<CancelOrderResponse>> {
+
+    override fun call(): ResponseWrapper<CancelOrderResponse> {
+        return callAsync().join()
+    }
+
+    override fun callAsync(): CompletableFuture<ResponseWrapper<CancelOrderResponse>> {
+        return localRestClient.cancelOrder(orderId, request);
+    }
+
+    override fun getRequest(): CancelOrderMessage {
+        return this.request
+    }
+
+    override fun getSignature(): Signature {
+        return this.request.signature
+    }
+
+    /**
+     * Call the API and poll for the status of original order with given order id until it is cancelled.
+     */
+    fun callAndPollOrder(): Order {
+        val ignored = call()
+        return OrderPoller(
+            this.localRestClient, this.orderId
+        ).poll(*defaultDesiredOrderState)
+    }
+
+    /**
+     * Call the API and poll for the status of original order with given order id until it is cancelled.
+     */
+    fun callAndPollOrderAsync(): CompletableFuture<Order> {
+        return this.callAsync().thenApplyAsync { _ ->
+            OrderPoller(this.localRestClient, orderId)
+        }.thenComposeAsync { it.pollAsync(*defaultDesiredOrderState) }
+    }
+
+    companion object {
+        val defaultDesiredOrderState = arrayOf(OrderState.Canceled)
+
+        @JvmStatic
+        fun build(
+            localRestClient: ReddioRestClient, orderId: Long, request: CancelOrderMessage
+        ): ReddioCancelOrderApi = ReddioCancelOrderApi(
+            localRestClient, orderId, request
+        )
+
+        @JvmStatic
+        fun cancelOrder(
+            localRestClient: ReddioRestClient, starkPrivateKey: String, orderId: Long
+        ): ReddioCancelOrderApi {
+            val starkExSigner = StarkExSigner(starkPrivateKey)
+            val starkKey = starkExSigner.getStarkKey()
+            val signature = starkExSigner.signCancelOrderMsg(orderId)
+            val request = CancelOrderMessage.of(starkKey, signature);
+            return build(localRestClient, orderId, request)
+        }
+    }
+}
